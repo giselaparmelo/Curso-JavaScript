d1f8651c0ad5b2a52a012687a18cb63dffdb269a
giselaparmelo <giselaparmelo@gmail.com>
2024-06-04 16:18:02 -0300

    Correcciones PreEntreg2

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -20,67 +20,47 @@ function mostrarCuotasSinInteres (cuotasDisponibles){
 /*Creo la función "mostrarPrecio" y uso un switch, por ser poco casos que funcionan con igualdad (no son necesarias comparaciones de mayor, menor, etc).
 El usuario puede consultar precio gracias a esta función.*/
 
-function mostrarPrecio(productoId) {
-    switch (productoId) {
-        case 1:
-            alert("El precio del producto 1 es $5500")            
-            break;
-        case 2:
-            alert("El  precio del producto 2 es $4500")
-            break;
+function iniciar() {
+    let respuesta = confirm("Quieres ver los productos?")
 
-        case 3:
-            alert("El  precio del producto 3 es $2100")            
-            break;
-
-        case 4:
-            alert("El  precio del producto 4 es $2350")
-            break;
-
-        case 5:
-            alert("El  precio del producto 5 es $2550")
-            break;
-        
-        case 6:
-            alert("El  precio del producto 6 es $4800")
-            break;
-
-        case 7:
-            alert("El  precio del producto 7 es $3900")
-            break;
-        
-        case 8:
-            alert("El  precio del producto 7 es $2800")
-            break;
-        
-        case 9:
-            alert("El  precio del producto 7 es $5100")
-            break;
-
-        case 10:
-            alert("El  precio del producto 7 es $3950")
-            break;
-
-        default:
-            console.warn("Por favor, ingresa un producto válido.")
-            break;
+    if (respuesta === true) {
+        mostrarProducto()
+    } else {
+        alert("Esperamos que vuelvas pronto!")
     }
-
 }
 
 function mostrarProducto() {
-    let respuesta = confirm("Quieres conocer el precio de este producto?")
-    if (respuesta === true) {
-        let productoId = prompt("Ingresa el Id del producto deseado")
-        if (productoId !== "") {
-            mostrarPrecio(parseInt(productoId))
+    const listado = productos.reduce((acc, prod) => acc += `${prod.id} - ${prod.nombre} - ${prod.precio} \n`, "");
+    
+    let productoId = prompt("Ingresa el Id del producto deseado: \n " + listado);
+    
+    const productoEncontrado = buscarProducto(parseInt(productoId))
+
+    if (productoEncontrado) {
+        alert(`El precio de ${productoEncontrado.nombre} es de ${productoEncontrado.precio}`);
+        const comprarProd = confirm("¿Desea comprar este producto?")
+        if(comprarProd) {
+            agregarAlCarrito(productoEncontrado)
+            const comprarMas = confirm("¿Desea comprar otro?")
+            if(comprarMas){
+                mostrarProducto()
+            } else {
+                alert("Hasta luego!")
+                return
+            }
         } else {
-            console.warn("Ingresa un producto válido, por favor.")
+            // finalizarCompra(); Más adelante se simulará.
         }
     } else {
-        console.log("Esperamos que vuelvas pronto!")
+        alert("Ingresa un producto válido, por favor.");
+        mostrarProducto();
     }
+}
 
+function agregarAlCarrito(prod){
+    carrito.push(prod)
+    console.log(carrito)
 }
 
 const carrito = [] // Inicializo carrito vacío.
@@ -126,3 +106,4 @@ function comprar () {
     }
 }
 
+iniciar();
